1dc12cbc486d8a5c9b04219254a41fd6c56bab41
kimseunggi <ninny9988@naver.com>
2025-05-16 16:12:17 +0900

    update config

--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/config/OpenApiConfig.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.dearwith.dearwith_backend;
+package com.dearwith.dearwith_backend.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
