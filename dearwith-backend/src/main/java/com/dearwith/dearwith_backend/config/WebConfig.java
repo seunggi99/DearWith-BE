eaf05a604991b4cfd6cdf8640659dd25cf95c5c4
kimseunggi <ninny9988@naver.com>
2025-05-16 22:52:37 +0900

    update config

--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/config/WebConfig.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/config/WebConfig.java
@@ -8,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://3.37.184.199:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+
 }
