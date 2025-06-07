85d0e5d114a888a078cad300864af544cff7014a
kimseunggi <ninny9988@naver.com>
2025-06-08 02:11:52 +0900

    Etc: 현재 버전 추가

--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/controller/HelloController.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/controller/HelloController.java
@@ -9,6 +9,6 @@ import java.util.Map;
 public class HelloController {
     @GetMapping("/")
     public Map<String, String> hello() {
-        return Map.of("message", "Hello, DearWith0606");
+        return Map.of("message", "DeatWith Api V20250608");
     }
 }
