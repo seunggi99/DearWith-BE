89bb6bcc8c3cdadd9cbad94dca4226261a2664ba
kimseunggi <ninny9988@naver.com>
2025-06-08 03:18:41 +0900

    Etc: 스웨거 문서 설명 추가

--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/auth/controller/AuthController.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.dearwith.dearwith_backend.auth.service.AuthService;
 import com.dearwith.dearwith_backend.auth.service.EmailVerificationService;
 import com.dearwith.dearwith_backend.common.exception.ErrorCode;
 import com.dearwith.dearwith_backend.common.exception.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,53 +25,80 @@ public class AuthController {
     }
 
 
-    // 1) 인증 코드 발송
+    @Operation(summary = "이메일 인증 코드 발송")
     @PostMapping("/signup/email/send")
     public ResponseEntity<Void> sendCode(@RequestBody @Valid EmailRequestDto request) {
         emailService.sendVerificationCode(request.getEmail());
         return ResponseEntity.ok().build();
     }
 
-    // 2) 인증 코드 검증
+    @Operation(summary = "이메일 인증 코드 검증")
     @PostMapping("/signup/email/verify")
     public ResponseEntity<Void> verifyCode(@RequestBody @Valid EmailVerifyRequestDto request) {
         emailService.verifyCode(request.getEmail(), request.getCode());
         return ResponseEntity.noContent().build();
     }
 
-    // 3) 회원가입
+    @Operation(summary = "회원가입" , description = "회원 가입시 필요한 Request" +
+            "{\n" +
+            "  \"email\": \"test@example.com\",\n" +
+            "  \"password\": \"testPassword\",\n" +
+            "  \"nickname\": \"테스트 닉네임\",\n" +
+            "  \"agreements\": [\n" +
+            "    {\n" +
+            "      \"type\": \"AGE_OVER_14\",\n" +
+            "      \"agreed\": true\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"type\": \"TERMS_OF_SERVICE\",\n" +
+            "      \"agreed\": true\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"type\": \"PERSONAL_INFORMATION\",\n" +
+            "      \"agreed\": true\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"type\": \"MARKETING_CONSENT\",\n" +
+            "      \"agreed\": false\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"type\": \"PUSH_NOTIFICATION\",\n" +
+            "      \"agreed\": false\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}")
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody @Valid SignUpRequestDto request){
         return ResponseEntity.ok(authService.signUp(request));
     }
 
-    // 4) 로그인
+    @Operation(summary = "로그인")
     @PostMapping("/signin")
     public ResponseEntity<SignInResponseDto> signIn(@RequestBody @Valid SignInRequestDto request){
         return ResponseEntity.ok(authService.signIn(request));
     }
 
-    // 토큰 유효 검사
+    @Operation(summary = "토큰 유효 검사")
     @PostMapping("/validate")
     public ResponseEntity<ErrorResponse> validateToken(@RequestBody @Valid JwtTokenDto tokenDto) {
         authService.validateToken(tokenDto);
         return ResponseEntity.ok(ErrorResponse.of(ErrorCode.TOKEN_SUCCESS));
     }
 
-    // 토큰 재발급
+    @Operation(summary = "토큰 재발급")
     @PostMapping("/refresh")
     public ResponseEntity<TokenReissueResponseDTO> refreshToken(@RequestBody @Valid JwtTokenDto tokenDto){
         return ResponseEntity.ok(authService.reissueToken(tokenDto));
     }
 
-    // 이메일 중복 체크
+    @Operation(summary = "이메일 중복 검사")
     @GetMapping("/check/email")
     public ResponseEntity<Void> checkEmailDuplicate(@RequestParam String email) {
         authService.validateDuplicateUserByEmail(email);
         return ResponseEntity.ok().build();
     }
 
-    // 닉네임 중복 체크
+    @Operation(summary = "닉네임 중복 검사")
     @GetMapping("/check/nickname")
     public ResponseEntity<Void> checkNicknameDuplicate(@RequestParam String nickname) {
         authService.validateDuplicateUserByNickname(nickname);
--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/user/controller/UserController.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.dearwith.dearwith_backend.user.domain.User;
 import com.dearwith.dearwith_backend.user.dto.UpdateNicknameRequestDto;
 import com.dearwith.dearwith_backend.user.dto.UserResponseDto;
 import com.dearwith.dearwith_backend.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,19 +23,19 @@ import java.util.stream.Collectors;
 public class UserController {
     private final UserService userService;
 
-    // 1) JWT 토큰으로 받은 현재 로그인한 회원 정보 조회
+    @Operation(summary = "현재 로그인 회원 정보 조회", description = "JWT 토큰으로 받은 현재 로그인한 회원 정보 조회")
     @GetMapping("/me")
     public ResponseEntity<UserResponseDto> getCurrentUser(@AuthenticationPrincipal CustomUserDetails principal) {
         return ResponseEntity.ok(userService.getCurrentUser(principal.getId()));
     }
 
-    // 2) 모든 회원 출력
+    @Operation(summary = "가입된 모든 회원 출력", description = "개발용 임시")
     @GetMapping("/all")
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    // 3) 회원 닉네임 변경
+    @Operation(summary = "회원 닉네임 변경")
     @PatchMapping("/me/nickname")
     public ResponseEntity<Void> updateNickname(@AuthenticationPrincipal CustomUserDetails principal,
                                                @RequestBody @Valid UpdateNicknameRequestDto req) {
@@ -42,7 +43,7 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    // 4) 회원 삭제
+    @Operation(summary = "회원 소프트 삭제(Status 변경)")
     @DeleteMapping("/me")
     public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal CustomUserDetails principal) {
         userService.deleteById(principal.getId());
