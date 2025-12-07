21ff31c2f4208ec7c18617b90b69a4d68def77fa
kimseunggi <ninny9988@naver.com>
2025-12-07 17:50:15 +0900

    Feat: /users/me 관리자 여부 추가

--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/user/dto/UserResponseDto.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/user/dto/UserResponseDto.java
@@ -12,11 +12,12 @@ public class UserResponseDto {
     private final UUID id;
     private final String email;
     private final String nickname;
+    private final Boolean isAdmin;
 
     public UserResponseDto(User u) {
         this.id = u.getId();
         this.email = u.getEmail();
         this.nickname = u.getNickname();
+        this.isAdmin = u.isAdmin();
     }
-
 }
