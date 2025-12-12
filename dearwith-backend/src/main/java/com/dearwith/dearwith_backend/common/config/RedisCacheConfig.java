51b75beb2dc50e24c5c736e67cf7bd78cc6db9dd
kimseunggi <ninny9988@naver.com>
2025-12-12 14:27:28 +0900

    Feat: RedisCache JSON 직렬화 변경

--- dearwith-backend/src/main/java/com/dearwith/dearwith_backend/common/config/RedisCacheConfig.java
+++ dearwith-backend/src/main/java/com/dearwith/dearwith_backend/common/config/RedisCacheConfig.java
@@ -1,5 +1,6 @@
 package com.dearwith.dearwith_backend.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +15,22 @@ import java.time.Duration;
 public class RedisCacheConfig {
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    public RedisCacheManager cacheManager(
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper
+    ) {
+        ObjectMapper om = objectMapper.copy()
+                .registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule())
+                .disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        RedisCacheConfiguration defaultConfig =
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .disableCachingNullValues()
-                        .entryTtl(Duration.ofHours(24));
+        var serializer = new org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer(om);
+
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+                )
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofHours(24));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(defaultConfig)
